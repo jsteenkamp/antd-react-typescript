276f802b69b3e8de1627961bb28a0d5cc6ebebb3
Johan Steenkamp <johan.steenkamp@gmail.com>
2022-12-12 07:27:33 +1300

    Update packages

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'wouter';
 import { NotFound } from './not-found';
 import { Start } from './start';
+// import 'antd/dist/reset.css';
 
 export const Views = (): JSX.Element => (
   <Switch>
